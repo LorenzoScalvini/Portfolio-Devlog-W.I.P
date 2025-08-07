83f3667070bcd7d7f01298537ff7ed9b470dc944
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-08-07 16:11:59 +0200

    Updates

--- src/pages/Me.jsx
+++
@@ -1,7 +0,0 @@
-import Contacts from '../components/Contacts/Contacts'
-import SkillsCards from '../components/SkillCards/SkillCards'
-export default function Me(){ 
- return (<>
- <Contacts />
- <SkillsCards /></>)
-}
--- src/pages/Projects.jsx
+++
@@ -1,6 +0,0 @@
-import CardGrid from '../components/CardGrid/CardGrid'
-export default function Projects() {
-    return (<>
-    <CardGrid />
-    </>)
-}
