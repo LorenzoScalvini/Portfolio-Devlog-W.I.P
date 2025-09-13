a4a3a8a41fb415dffd506c1880b1fa680e480736
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-09-13 07:44:56 +0200

    Update: Footer styling and links data

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,19 @@ import styles from "./Footer.module.css";
 import instagramLogo from "../../assets/Logos/InstagramLogo.svg";
 import githubLogo from "../../assets/Logos/GithubLogo.svg";
 import linkedinLogo from "../../assets/Logos/linkedinLogo.svg";
+import item1 from "../../assets/footerdeco/item1.gif";
+import item2 from "../../assets/footerdeco/item2.gif";
+import item3 from "../../assets/footerdeco/item3.gif";
+import item4 from "../../assets/footerdeco/item4.gif";
+import item5 from "../../assets/footerdeco/item5.gif";
+
+const pokemonImages = {
+  left: item1,
+  center: item2,
+  centerRight: item4,
+  centerLeft: item5,
+  right: item3,
+};
 
 const footerTexts = {
   infoTitle: "Informazioni",
@@ -35,14 +48,40 @@ const footerTexts = {
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <div className={styles.pokemonContainer}>
+        <img src={pokemonImages.left} alt="Pokemon sinistro" className={styles.pokemonImage} />
+        <img src={pokemonImages.center} alt="Pokemon centrale" className={styles.pokemonImage} />
+        <img src={pokemonImages.centerLeft} alt="Pokemon centro-sinistro" className={styles.pokemonImage} />
+        <img src={pokemonImages.centerRight} alt="Pokemon centro-destro" className={styles.pokemonImage} />
+        <img src={pokemonImages.right} alt="Pokemon destro" className={styles.pokemonImage} />
+      </div>
+      
       <div className={styles.container}>
         <div className={styles.footerContent}>
-          {/* Informazioni */}
           <div className={styles.footerSection}>
             <h3 className={styles.footerTitle}>{footerTexts.infoTitle}</h3>
             <ul className={styles.contactInfo}>
               {footerTexts.contactInfo.map((info, index) => (
                 <li key={index}>{info}</li>
+              ))}
+            </ul>
+          </div>
+          
+          <div className={styles.footerSection}>
+            <h3 className={styles.footerTitle}>{footerTexts.followTitle}</h3>
+            <ul className={styles.socialLinks}>
+              {footerTexts.socialLinks.map((link, index) => (
+                <li key={index}>
+                  <a 
+                    href={link.href} 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className={styles.socialLink}
+                  >
+                    <img src={link.icon} alt={link.label} className={styles.socialIcon} />
+                    <span>{link.label}</span>
+                  </a>
+                </li>
               ))}
             </ul>
           </div>
